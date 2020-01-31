893974916a6b5943e2c0dfd5b830b755a54b5e65
yoshicon <39690796+yoshicon@users.noreply.github.com>
2020-01-31 17:59:18 +0900

    Add files via upload

---
+++ javabook/src/javabook/ch5/Hr.java
@@ -0,0 +1,12 @@
+package javabook.ch5;
+
+public class Hr{
+    void Go(){
+        Hi hel1 = new Hi();
+        hel1.prt();
+    }
+    public static void main(String[] args) {
+        Hr gd = new Hr();
+        gd.Go();
+    }
+}
