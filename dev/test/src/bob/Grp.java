e495f8d20c7b68cb9c4df0c0308fc9435acd2c1d
yoshicon <39690796+yoshicon@users.noreply.github.com>
2020-02-06 13:02:29 +0900

    Add files via upload

---
+++ dev/test/src/bob/Grp.java
@@ -0,0 +1,47 @@
+package bob;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class Grp extends JFrame implements ActionListener{
+	
+	JButton bt1 = new JButton("란-듐");
+	JPanel lap = new JPanel();
+	JLabel jl = new JLabel("머머금?");
+	
+	public Grp() {
+		setTitle("점심");
+		
+		BorderLayout lay = new BorderLayout();
+		setLayout(lay);
+		
+		bt1.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				setM();
+			}
+		});
+		
+		add(bt1, BorderLayout.SOUTH);
+		add(jl, BorderLayout.CENTER);
+		jl.setHorizontalAlignment(JLabel.CENTER);
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(200, 200);
+		setVisible(true);
+	}
+	
+	public void setM() {
+		EvenMoham.mn = Menu.Menu(EvenMoham.babo);
+		jl.setText(EvenMoham.mn);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
